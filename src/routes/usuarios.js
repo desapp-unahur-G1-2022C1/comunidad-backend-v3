a39c8e1c1b910c1637343059431a30f9c76c19f5
PerroSanto <msguser@gmail.com>
2022-12-07 10:59:18 -0300

    configuracion de enviroment

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -10,7 +10,7 @@ import { validateToken } from '../middlewares/validador';
 
 const router = express.Router();
 
-//router.get('/', withErrorHandling(getAll));
+router.get('/', withErrorHandling(getAll));
 router.post('/signin', withErrorHandling(signIn));
 router.post('/signup', withErrorHandling(signUp));
 //router.delete('/:id', withErrorHandling(deleteUsuario));
